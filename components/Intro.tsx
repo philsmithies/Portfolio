d857a520c95ea23f8e998d03d385749fa1aaa156
Phil Smithies <philsmithies@gmail.com>
2021-09-09 05:19:39 +0200

    intro component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -12,16 +12,26 @@ import {
 const Intro = () => {
   const { toggleColorMode } = useColorMode();
   const formBackground = useColorModeValue("gray.100", "gray.700");
+  const fontColor = useColorModeValue("grey", "white");
 
   return (
     <Flex p={12} rounded={6} flexDirection={"column"}>
-      <Heading color="#1EA1A0" mb={6} as="h3" size="lg">
+      <Heading color="#1EA1A0" mb={1} as="h3" size="lg">
         Hi there!, I'm-
       </Heading>
-      <Heading as="h1" size="4xl">
+      <Heading as="h1" mb={2} size="4xl">
         Phil Smithies.
       </Heading>
-      <Text fontSize="lg">(4xl) In love with React & Next</Text>
+      <Text fontSize="2xl">
+        Full Stack Web Developer.{" "}
+        <Text color={fontColor}>
+          Specialising in building clean, and engaging applications on the web.
+        </Text>
+      </Text>
+      <br />
+      <Text fontSize="2xl">🚀 Currently working on this portfolio....</Text>
+      <br />
+      <Text fontSize="2xl">🌱 Makers Academy Alumni</Text>
     </Flex>
   );
 };
