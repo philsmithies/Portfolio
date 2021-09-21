10ed740b86f674c616cbefe4027e9037571ad06f
Phil Smithies <philsmithies@gmail.com>
2021-09-21 02:10:37 +0200

    background colour on div index

--- pages/index.jsx
+++ pages/index.jsx
@@ -31,91 +31,101 @@ const IndexPage = () => {
   const [isSmallerScreen] = useMediaQuery(`(max-width: 380px)`);
 
   return (
-    <Flex
-      pt={"5%"}
-      alignItems="center"
-      flexDirection="column"
-      background={"gray.800"}
-    >
-      <Head>
-        <title>Phil Smithies: Dev</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Box width={!isSmallerScreen ? "80%" : "90%"} maxWidth="800px" mb={"15%"}>
-        <Intro />
-        <About />
-
-        <Flex
-          className={styles.flexDirectionDiv}
-          justifyContent="center"
-          alignItems="center"
-          mt={"10%"}
-          mb="15%"
+    <Box backgroundColor="#1a202c">
+      <Flex
+        pt={"5%"}
+        alignItems="center"
+        flexDirection="column"
+        background={"gray.800"}
+        maxWidth="98%"
+      >
+        <Head>
+          <title>Phil Smithies: Dev</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Box
+          width={!isSmallerScreen ? "80%" : "90%"}
+          maxWidth="800px"
+          mb={"15%"}
         >
-          <Flex direction="column" pt={10} mr={!isSmallerScreen ? "1em" : 0}>
-            <Box alignSelf="flex-start">
-              <Heading
-                fontSize={!isSmallerScreen ? "52px" : "35px"}
-                mr={!isSmallerScreen ? 10 : 0}
-                color="white"
-              >
-                View my projects
-              </Heading>
-              <Text color="white">
-                All of my projects can be seen{" "}
-                <Link color={"teal.400"} href="/projects">
-                  here
-                </Link>
-              </Text>
-            </Box>
-            <Box mt={{ sm: 0, md: 10 }}>
-              <Card
-                imageUrl={"./preview_map.png"}
-                imageAlt={"Shuttr - A Photo Location Discovery Application"}
-                title={"Shuttr"}
-                body={
-                  "A Photo Sharing and Location Discovery Application combining the Google Maps API with image sharing."
-                }
-                link={"https://www.github.com/philsmithies/shuttr"}
-                react={true}
-                javascript={true}
-                node={true}
-              />
-            </Box>
+          <Intro />
+          <About />
+
+          <Flex
+            className={styles.flexDirectionDiv}
+            justifyContent="center"
+            alignItems="center"
+            mt={"10%"}
+            mb="15%"
+          >
+            <Flex direction="column" pt={10} mr={!isSmallerScreen ? "1em" : 0}>
+              <Box alignSelf="flex-start">
+                <Heading
+                  fontSize={!isSmallerScreen ? "52px" : "35px"}
+                  mr={!isSmallerScreen ? 10 : 0}
+                  color="white"
+                >
+                  View my projects
+                </Heading>
+                <Text color="white">
+                  All of my projects can be seen{" "}
+                  <Link color={"teal.400"} href="/projects">
+                    here
+                  </Link>
+                </Text>
+              </Box>
+              <Box mt={{ sm: 0, md: 10 }}>
+                <Card
+                  imageUrl={"./preview_map.png"}
+                  imageAlt={"Shuttr - A Photo Location Discovery Application"}
+                  title={"Shuttr"}
+                  body={
+                    "A Photo Sharing and Location Discovery Application combining the Google Maps API with image sharing."
+                  }
+                  link={"https://www.github.com/philsmithies/shuttr"}
+                  react={true}
+                  javascript={true}
+                  node={true}
+                />
+              </Box>
+            </Flex>
+            <Flex direction="column" mr={!isSmallerScreen ? "1em" : 0}>
+              <Box>
+                <Card
+                  imageUrl="./chitter.png"
+                  imageAlt={"Chitter - A Twitter Clone"}
+                  title={"Chitter"}
+                  body={
+                    "A Popular Social Media Clone built using the MERN Stack."
+                  }
+                  link={"https://www.github.com/philsmithies/chitter-react"}
+                  react={true}
+                  javascript={true}
+                  node={true}
+                />
+              </Box>
+              <Box>
+                <Card
+                  imageUrl={"./cv_generator.png"}
+                  imageAlt={"CV Generator"}
+                  title={"CV Generator"}
+                  body={
+                    "A dynamic CV Generator written in React, written utilizing React Hooks"
+                  }
+                  link={"https://github.com/philsmithies/cv-generator/"}
+                  react={true}
+                  javascript={true}
+                />
+              </Box>
+            </Flex>
           </Flex>
-          <Flex direction="column" mr={!isSmallerScreen ? "1em" : 0}>
-            <Box>
-              <Card
-                imageUrl="./chitter.png"
-                imageAlt={"Chitter - A Twitter Clone"}
-                title={"Chitter"}
-                body={
-                  "A Popular Social Media Clone built using the MERN Stack."
-                }
-                link={"https://www.github.com/philsmithies/chitter-react"}
-                react={true}
-                javascript={true}
-                node={true}
-              />
-            </Box>
-            <Box>
-              <Card
-                imageUrl={"./cv_generator.png"}
-                imageAlt={"CV Generator"}
-                title={"CV Generator"}
-                body={
-                  "A dynamic CV Generator written in React, written utilizing React Hooks"
-                }
-                link={"https://github.com/philsmithies/cv-generator/"}
-                react={true}
-                javascript={true}
-              />
-            </Box>
-          </Flex>
-        </Flex>
-        <Contact />
-      </Box>
-    </Flex>
+          <Contact />
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 
