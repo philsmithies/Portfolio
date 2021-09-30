64c576555d253c93cdda85f31f8dd5d7be4d125b
Phil Smithies <philsmithies@gmail.com>
2021-09-30 06:03:28 +0200

    navbar continued

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="bg-gray-900 flex flex-row items-center justify-between">
+    <div className="bg-gray-900 flex flex-row items-center justify-between pb-2">
       <div>
         <Link href="/" passHref>
           <h2
-            className="text-5xl font-bold text-yellow-400 font-body ml-6 cursor-auto text-red-300"
+            className="text-4xl font-bold text-yellow-400 font-body ml-6 cursor-auto"
             // _hover={{
             //   cursor: "pointer",
             //   color: "teal.500",
@@ -14,18 +14,19 @@ const Navbar = () => {
             // color={"teal.100"}
             // ml="6"
           >
-            P
+            <span className="text-green-300 text-5xl">&#123;</span>PS
+            <span className="text-green-300 text-5xl">&#125;</span>
           </h2>
         </Link>
       </div>
       <div className="p-4 justify-self-end">
         <Link href="/" passHref>
-          <button className="bg-gray-800 hover:bg-gray-600 text-white font-semibold py-2 px-4 hover:border-gray-400 rounded shadow mr-5">
+          <button className="bg-gray-800 hover:bg-gray-600 text-white font-semibold py-2 px-4 hover:border-gray-400 rounded shadow mr-2">
             Home
           </button>
         </Link>
         <Link href="/projects" passHref>
-          <button className="bg-gray-800 hover:bg-gray-600 text-white font-semibold py-2 px-4 hover:border-gray-400 rounded shadow mr-5">
+          <button className="bg-gray-800 hover:bg-gray-600 text-white font-semibold py-2 px-4 hover:border-gray-400 rounded shadow">
             Projects
           </button>
         </Link>
