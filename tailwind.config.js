42e15c744368b790a1b095ff781358db2b06439d
Phil Smithies <philsmithies@gmail.com>
2021-10-01 01:45:19 +0200

    config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,25 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      display: ["Oswald", "Arial"],
-      body: ['"Open Sans"', "Helvetica"],
+    extend: {
+      fontFamily: {
+        display: ["Oswald", "Arial"],
+        body: ['"Open Sans"', "Helvetica"],
+      },
+      textColor: {
+        teal100: "#B2F5EA",
+        teal300: "#4FD1C5",
+        teal600: "#2C7A7B",
+        secondary: "#ECC94B",
+      },
+      backgroundColor: {
+        teal100: "#B2F5EA",
+        teal300: "#4FD1C5",
+        teal600: "#2C7A7B",
+        secondary: "#ECC94B",
+        backgroundColor: "#1a202c",
+      },
     },
-    textColor: {
-      teal100: "#B2F5EA",
-      teal300: "#4FD1C5",
-      teal600: "#2C7A7B",
-      secondary: "#ECC94B",
-    },
-    backgroundColor: (theme) => ({
-      teal100: "#B2F5EA",
-      teal300: "#4FD1C5",
-      teal600: "#2C7A7B",
-      secondary: "#ECC94B",
-    }),
-    extend: {},
   },
   variants: {
     extend: {},
